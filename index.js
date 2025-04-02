2fbb3bb1911d36c1e80417ea876b3f5764835c66
Godwin0397 <godveenpaul@gmail.com>
2025-04-02 09:48:34 +0530

    server started

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-const mongoose = require('mongoose');
-const express = require('express');
-const config = require('./utlis/config');
+// importing the required modules
+const mongoose = require("mongoose");
+const config = require("./utlis/config");
+const app = require("./app");
 
-
-
-mongoose.connect(config.mongodb_url).then(() => {
-    console.log('MongoDB connected successfully');
-}).catch((err) => { 
-    console.log('MongoDB connection failed', err);
-})
+// setting the mongoDB connection string
+mongoose
+  .connect(config.mongodb_url)
+  .then(() => {
+    console.log("MongoDB connected successfully");
+    // starting the server
+    app.listen(config.port, () => {
+      console.log(`Server is running on port ${config.port}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB connection failed", err);
+  });
